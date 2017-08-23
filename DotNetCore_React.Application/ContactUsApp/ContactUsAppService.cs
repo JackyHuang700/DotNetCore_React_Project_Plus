dcc676349dd360e2285954465aa1e1862e5f839c
Jacky <jacky8595125@gmail.com>
2017-08-23 14:31:56 +0800

    實作 ContactUsApp

--- DotNetCore_React.Application/ContactUsApp/ContactUsAppService.cs
+++ DotNetCore_React.Application/ContactUsApp/ContactUsAppService.cs
@@ -34,17 +34,46 @@ namespace DotNetCore_React.Application.ContactUsApp
 
         public List<ContactUsDto> GetAll()
         {
-            throw new NotImplementedException();
+            var a = _repository.GetAllList();
+            var newsDtoList = Mapper.Map<List<ContactUsDto>>(a);
+
+            return newsDtoList;
         }
 
         public ContactUsDto GetSingle(string id)
         {
-            throw new NotImplementedException();
+            //轉換Guid
+            Guid guid;
+            Guid.TryParse(id, out guid);
+            //抓取主表
+            var a = _repository.Get(guid);
+            var newsDto = Mapper.Map<ContactUsDto>(a);
+
+            return newsDto;
         }
 
         public Dictionary<string, object> Update(ContactUsDto News)
         {
-            throw new NotImplementedException();
+            //11.6.1	狀態只能修改為無須回覆(2)，已回覆跟未回覆都是系統判斷
+            var myJson = new Dictionary<string, object>()
+            {
+                {"success",false },
+                {"message",null  }
+            };
+
+            //更新主表
+            var newsDB = _repository.Get(News.Id);
+            newsDB = Mapper.Map<ContactUsDto, ContactUs>(News, newsDB);
+
+            newsDB.UpdateDate = DateTime.Now;
+            _repository.Update(newsDB);
+            var news_effect = _repository.Save() > 0;
+
+            var success_effect = news_effect;
+            myJson["success"] = success_effect;
+            myJson["message"] = success_effect ? "更新成功" : "更新失敗";
+
+            return myJson;
         }
     }
 }
