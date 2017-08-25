372509434d3bd84f9b058615ff0c47f7698758cf
Jacky <jacky8595125@gmail.com>
2017-08-25 20:35:47 +0800

    fix DotNetCore_React.EntityFrameworkCore add DB

--- DotNetCore_React.EntityFrameworkCore/DotNetCore_ReactDBContext.cs
+++ DotNetCore_React.EntityFrameworkCore/DotNetCore_ReactDBContext.cs
@@ -21,7 +21,21 @@ namespace DotNetCore_React.EntityFrameworkCore
         public DbSet<News> News { get; set; }
         public DbSet<News_Lan> News_Lan { get; set; }
         public DbSet<Sys_Language> Sys_Languages { get; set; }
+        public DbSet<AboutUs> AboutUs { get; set; }
+        public DbSet<AboutUs_Category> AboutUs_Category { get; set; }
+        public DbSet<AboutUs_Lan> AboutUs_Lan { get; set; }
+        public DbSet<ContactUs> ContactUs { get; set; }
+        public DbSet<ContactUs_Category> ContactUs_Category { get; set; }
+        public DbSet<Location> Location { get; set; }
+        public DbSet<Location_Image> Location_Image { get; set; }
+        public DbSet<Location_Lan> Location_Lan { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Product_Category> Product_Category { get; set; }
+        public DbSet<Product_Category_Lan> Product_Category_Lan { get; set; }
+        public DbSet<Product_Image> Product_Image { get; set; }
+        public DbSet<Product_Lan> Product_Lan { get; set; }
 
+        
 
 
         protected override void OnModelCreating(ModelBuilder builder)
