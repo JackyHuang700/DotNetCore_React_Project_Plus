28d0b2de173ca85d34b7492b99662dd0e4426595
Jacky <jacky8595125@gmail.com>
2017-08-25 20:35:13 +0800

    add AboutUs_Delete

--- DotNetCore_React/ClientApp/routes.js
+++ DotNetCore_React/ClientApp/routes.js
@@ -31,7 +31,7 @@ import News_View from './views/Backend/News/News_View'
 
 import AboutUs_View from './views/Backend/AboutUs/AboutUs_View'
 import AboutUs_Create from './views/Backend/AboutUs/AboutUs_Create'
-// import AboutUs_Delete from './views/Backend/AboutUs/AboutUs_Delete'
+import AboutUs_Delete from './views/Backend/AboutUs/AboutUs_Delete'
 // import AboutUs_Edit from './views/Backend/AboutUs/AboutUs_Edit'
 
 
@@ -40,30 +40,30 @@ import AboutUs_Create from './views/Backend/AboutUs/AboutUs_Create'
   * 已知Breadcrumb�呼�這隻
   */
 export default (
-      <Switch>
-        <FrontRoute path='/' exact component={Home} />
-        <FrontRoute path='/Index' exact component={Home} />
-        <LoginRoute path='/login' component={Login} />
-        <LoginRoute path='/forgot' component={Forgot} />
-        <EmptyRoute path='/changePwd' component={changePwd} />
-        <BackendRoute path='/dashboard' component={Dashboard} />
-        <BackendRoute path='/Role' exact component={Role_View} />
-        <BackendRoute path='/Role/Create' component={Role_Create} />
-        <BackendRoute path='/Role/Delete/:id' component={Role_Delete} />
-        <BackendRoute path='/Role/Edit/:id/:edit' component={Role_Edit} />
-        <BackendRoute path='/User' exact component={User_View} />
-        <BackendRoute path='/User/Create' component={User_Create} />
-        <BackendRoute path='/User/Delete/:id' component={User_Delete} />
-        <BackendRoute path='/User/Edit/:id/:edit' component={User_Edit} />
-        <BackendRoute path='/User/Personal_Edit/:userName' component={User_Personal_Edit} />
-        <BackendRoute path='/News' exact component={News_View} />
-        <BackendRoute path='/News/Create' component={News_Create} />
-        <BackendRoute path='/News/Delete/:id/:delete' component={News_Delete} />
-        <BackendRoute path='/News/Edit/:id' component={News_Edit} />
-        <BackendRoute path='/AboutUs' exact component={AboutUs_View} />
-        <BackendRoute path='/AboutUs/Create' component={AboutUs_Create} />
-        {/* <BackendRoute path='/AboutUs/Delete/:id/:delete' component={AboutUs_Delete} />
-        <BackendRoute path='/AboutUs/Edit/:id' component={AboutUs_Edit} /> */}
-        <Route render={() => <h3>No Match Route</h3>} />
-      </Switch>
+  <Switch>
+    <FrontRoute path='/' exact component={Home} />
+    <FrontRoute path='/Index' exact component={Home} />
+    <LoginRoute path='/login' component={Login} />
+    <LoginRoute path='/forgot' component={Forgot} />
+    <EmptyRoute path='/changePwd' component={changePwd} />
+    <BackendRoute path='/dashboard' component={Dashboard} />
+    <BackendRoute path='/Role' exact component={Role_View} />
+    <BackendRoute path='/Role/Create' component={Role_Create} />
+    <BackendRoute path='/Role/Delete/:id' component={Role_Delete} />
+    <BackendRoute path='/Role/Edit/:id/:edit' component={Role_Edit} />
+    <BackendRoute path='/User' exact component={User_View} />
+    <BackendRoute path='/User/Create' component={User_Create} />
+    <BackendRoute path='/User/Delete/:id' component={User_Delete} />
+    <BackendRoute path='/User/Edit/:id/:edit' component={User_Edit} />
+    <BackendRoute path='/User/Personal_Edit/:userName' component={User_Personal_Edit} />
+    <BackendRoute path='/News' exact component={News_View} />
+    <BackendRoute path='/News/Create' component={News_Create} />
+    <BackendRoute path='/News/Delete/:id/:delete' component={News_Delete} />
+    <BackendRoute path='/News/Edit/:id' component={News_Edit} />
+    <BackendRoute path='/AboutUs' exact component={AboutUs_View} />
+    <BackendRoute path='/AboutUs/Create' component={AboutUs_Create} />
+    <BackendRoute path='/AboutUs/Delete/:id/:delete' component={AboutUs_Delete} />
+    {/*<BackendRoute path='/AboutUs/Edit/:id' component={AboutUs_Edit} /> */}
+    <Route render={() => <h3>No Match Route</h3>} />
+  </Switch>
 )
