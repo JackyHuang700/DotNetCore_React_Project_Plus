8f83eac225f5e429697e749ae2c248a226e8b740
Jacky <jacky8595125@gmail.com>
2017-08-29 14:29:10 +0800

    fix Product_Edit 附表無法編輯

--- DotNetCore_React/ClientApp/views/Backend/Product/Product_Edit.js
+++ DotNetCore_React/ClientApp/views/Backend/Product/Product_Edit.js
@@ -130,6 +130,7 @@ class Product_Edit extends Component {
                                         display={this.props.display_content}
                                         required={this.props.required_content}
                                         validMessage={{ required: '內容 is reduired.' }}
+                                        onInput={this.HandleInputChange_By_LanList}
                                         value={this.state.viewModel.lanList[`${index}`].languageId}
                                         placeholder="languageId" />
 
@@ -141,6 +142,7 @@ class Product_Edit extends Component {
                                         display={this.props.display_title}
                                         required={this.props.required_title}
                                         validMessage={{ required: '標題 is reduired.' }}
+                                          onInput={this.HandleInputChange_By_LanList}
                                         value={this.state.viewModel.lanList[`${index}`].title}
                                         placeholder="title" />
 
@@ -152,6 +154,7 @@ class Product_Edit extends Component {
                                         display={this.props.display_subTitle}
                                         required={this.props.required_subTitle}
                                         validMessage={{ required: '副標題 is reduired.' }}
+                                          onInput={this.HandleInputChange_By_LanList}
                                         value={this.state.viewModel.lanList[`${index}`].subTitle}
                                         placeholder="subTitle" />
 
@@ -165,6 +168,7 @@ class Product_Edit extends Component {
                                         display={this.props.display_name}
                                         required={this.props.required_name}
                                         validMessage={{ required: '名稱 is reduired.' }}
+                                          onInput={this.HandleInputChange_By_LanList}
                                         value={this.state.viewModel.lanList[`${index}`].name}
                                         placeholder="name" />
 
@@ -176,6 +180,7 @@ class Product_Edit extends Component {
                                         display={this.props.display_content}
                                         required={this.props.required_content}
                                         validMessage={{ required: '內容 is reduired.' }}
+                                          onInput={this.HandleInputChange_By_LanList}
                                         value={this.state.viewModel.lanList[`${index}`].content}
                                         placeholder="content" />
 
