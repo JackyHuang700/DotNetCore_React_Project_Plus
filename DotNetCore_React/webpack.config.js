1d6cfb7fb2c9d649e0da1b4656b9aecc028a4b33
Jacky <jacky8595125@gmail.com>
2017-08-29 23:03:00 +0800

    提取共用JS模組 (加快打包速度)

--- DotNetCore_React/webpack.config.js
+++ DotNetCore_React/webpack.config.js
@@ -51,70 +51,101 @@ const publicPath = `${sourcePath}/dist/`;
 
 //https://doc.webpack-china.org/guides/production/
 module.exports = (env) => {
-   return [{
+    return [{
         // devtool: 'source-map',
-    // devtool: 'cheap-eval-source-map',
-    // devtool: 'eval-source-map',
-    devtool: 'cheap-module-eval-source-map',
-    entry: {
-        'App': [
-            'babel-polyfill',
-            'react-hot-loader/patch',
-            `./ClientApp/App.js`
-        ]
-    },
-    output: {
-        path: path.join(__dirname, bundleOutputDir),
-        filename: '[name].js',
-        publicPath: '/dist/'
-    },
-    // output: {
-    //     filename: 'bundle.js',
-    //     path: `${publicPath}/js/`
-    // },
-    context: __dirname,
-    devServer: {
-        contentBase: publicPath,
-        compress: true,
-        inline: true,
-        hot: true,
-        proxy: {
-            '*': {
-                // target: 'http://localhost:5000',
-                target: 'http://localhost:60658',
-            }
+        // devtool: 'cheap-eval-source-map',
+        // devtool: 'eval-source-map',
+        devtool: 'cheap-module-eval-source-map',
+        entry: {
+            'App': [
+                // 'babel-polyfill',
+                // 'react-hot-loader/patch',
+                `./ClientApp/App.js`
+            ],
+            "commons": [
+                'react',
+                'react-dom',
+                'react-router-dom',
+                'babel-polyfill',
+                'react-hot-loader/patch',
+                "react-hot-loader",
+                "axios",
+                "react-easyform",
+                "history",
+                "reactstrap", //0.74MB
+                "bootstrap",
+                "event-source-polyfill",
+                // "react-scripts",
+                // "aspnet-webpack",
+                // "aspnet-webpack-react",
+                "./node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css",
+            ],
         },
-        // watch: true,
-        // devtool: "eval",
-        port: 8080,
-    },
-    module: {
-        rules: [{
+        output: {
+            path: path.join(__dirname, bundleOutputDir),
+            filename: '[name].js',
+            publicPath: '/dist/'
+        },
+        // output: {
+        //     filename: 'bundle.js',
+        //     path: `${publicPath}/js/`
+        // },
+        context: __dirname,
+        devServer: {
+            contentBase: publicPath,
+            compress: true,
+            inline: true,
+            hot: true,
+            proxy: {
+                '*': {
+                    // target: 'http://localhost:5000',
+                    target: 'http://localhost:60658',
+                }
+            },
+            // watch: true,
+            // devtool: "eval",
+            port: 8080,
+        },
+        module: {
+            rules: [{
                 test: /\.js$/,
                 use: [{ loader: 'babel-loader' }],
                 exclude: /node_modules/
             },
             { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.scss$/, use: ['style-loader', 'css-loader','sass-loader'] },
+            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
             { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' },
             { test: /.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
-        ]
-    },
-    plugins: [
-      
-        // new webpack.DefinePlugin({
+            ]
+        },
+        plugins: [
+
+            // new webpack.DefinePlugin({
             // 'process.env.NODE_ENV': JSON.stringify('production')
-        // }),
-        // new webpack.SourceMapDevToolPlugin({
-        //     filename: '[file].map',
-        //      exclude: ['vendor.js',],
-        //       columns: false
-        // }),
-        // new webpack.optimize.UglifyJsPlugin({
-        //     sourceMap: true,
-        // }),
-    ].concat((env) === "dev" ? [
-        new webpack.HotModuleReplacementPlugin(),
-    ] : [])
-   }];
+            // }),
+            // new webpack.SourceMapDevToolPlugin({
+            //     filename: '[file].map',
+            //      exclude: ['vendor.js',],
+            //       columns: false
+            // }),
+            // new webpack.optimize.UglifyJsPlugin({
+            //     sourceMap: true,
+            // }),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: "commons",
+                // (the commons chunk name)
+
+                filename: "commons.js",
+                // (the filename of the commons chunk)
+
+                // minChunks: 3,
+                // (Modules must be shared between 3 entries)
+
+                // chunks: ["pageA", "pageB"],
+                // (Only use these entries)
+            })
+        ].concat((env) === "dev" ? [
+            new webpack.HotModuleReplacementPlugin(),
+        ] : [])
+    }];
 };
